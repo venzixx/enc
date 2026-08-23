95c36c2e7d7b43fd448ac1d7f17e2c256c407b9a
Sidharth Samantaray <230301120185@centurionuniv.edu.in>
2026-08-23 21:50:42 +0530

    chore: purge makeitaquote clone, scripts, builder_upload, and legacy text files

--- makeitaquote-main/lib/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  API_URL: 'https://api.voids.top/fakequote',
-  BETA_API_URL: 'https://api.voids.top/fakequotebeta'
-};
--- scripts/check_jsons.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-async function main() {
-    console.log("I cannot run this because the client is generated for postgres.");
-    console.log("I will check the exported JSON files instead if they still exist.");
-}
-main();
